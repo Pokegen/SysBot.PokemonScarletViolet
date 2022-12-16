90c509c0d5e8015e031277cd3380e69b9881dc94
Yukine <devyukine@gmx.de>
2022-12-16 18:22:56 +0100

    feat: add docker builds for arm64, amd64 & alpine

--- SysBot.Pokemon.ConsoleApp/Program.cs
+++ SysBot.Pokemon.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.IO;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using PKHeX.Core;
@@ -9,129 +10,126 @@ using SysBot.Pokemon.Z3;
 
 namespace SysBot.Pokemon.ConsoleApp
 {
-    public static class Program
-    {
-        private static string ConfigPath = @"config.json";
+	public static class Program
+	{
+		private static string ConfigPath = @"config.json";
 
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("Starting up...");
-            if (args.Length > 1)
-                Console.WriteLine("This program does not support command line arguments.");
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Starting up...");
+			if (args.Length > 1)
+				Console.WriteLine("This program does not support command line arguments.");
 
-            if (!File.Exists(ConfigPath))
-            {
-                ExitNoConfig();
-                return;
-            }
+			if (!File.Exists(ConfigPath))
+			{
+				ExitNoConfig();
+				return;
+			}
 
-            try
-            {
-                var lines = File.ReadAllText(ConfigPath);
-                var cfg = JsonConvert.DeserializeObject<ProgramConfig>(lines, GetSettings()) ?? new ProgramConfig();
-                PokeTradeBot.SeedChecker = new Z3SeedSearchHandler<PK8>();
-                BotContainer.RunBots(cfg);
-            }
+			try
+			{
+				var lines = File.ReadAllText(ConfigPath);
+				var cfg = JsonConvert.DeserializeObject<ProgramConfig>(lines, GetSettings()) ?? new ProgramConfig();
+				PokeTradeBot.SeedChecker = new Z3SeedSearchHandler<PK8>();
+				BotContainer.RunBots(cfg).Wait();
+			}
 #pragma warning disable CA1031 // Do not catch general exception types
-            catch
+			catch
 #pragma warning restore CA1031 // Do not catch general exception types
-            {
-                Console.WriteLine("Unable to start bots with saved config file. Please copy your config from the WinForms project or delete it and reconfigure.");
-                Console.ReadKey();
-            }
-        }
+			{
+				Console.WriteLine("Unable to start bots with saved config file. Please copy your config from the WinForms project or delete it and reconfigure.");
+			}
+		}
 
-        private static void ExitNoConfig()
-        {
-            var bot = new PokeBotState { Connection = new SwitchConnectionConfig { IP = "192.168.0.1", Port = 6000 }, InitialRoutine = PokeRoutineType.FlexTrade };
-            var cfg = new ProgramConfig { Bots = new[] { bot } };
-            var created = JsonConvert.SerializeObject(cfg, GetSettings());
-            File.WriteAllText(ConfigPath, created);
-            Console.WriteLine("Created new config file since none was found in the program's path. Please configure it and restart the program.");
-            Console.WriteLine("It is suggested to configure this config file using the GUI project if possible, as it will help you assign values correctly.");
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
-        }
+		private static void ExitNoConfig()
+		{
+			var bot = new PokeBotState { Connection = new SwitchConnectionConfig { IP = "192.168.0.1", Port = 6000 }, InitialRoutine = PokeRoutineType.FlexTrade };
+			var cfg = new ProgramConfig { Bots = new[] { bot } };
+			var created = JsonConvert.SerializeObject(cfg, GetSettings());
+			File.WriteAllText(ConfigPath, created);
+			Console.WriteLine("Created new config file since none was found in the program's path. Please configure it and restart the program.");
+			Console.WriteLine("It is suggested to configure this config file using the GUI project if possible, as it will help you assign values correctly.");
+			Console.WriteLine("Press any key to exit.");
+			Console.ReadKey();
+		}
 
-        private static JsonSerializerSettings GetSettings() => new()
-        {
-            Formatting = Formatting.Indented,
-            DefaultValueHandling = DefaultValueHandling.Include,
-            NullValueHandling = NullValueHandling.Ignore,
-            ContractResolver = new SerializableExpandableContractResolver(),
-        };
+		private static JsonSerializerSettings GetSettings() => new()
+		{
+			Formatting = Formatting.Indented,
+			DefaultValueHandling = DefaultValueHandling.Include,
+			NullValueHandling = NullValueHandling.Ignore,
+			ContractResolver = new SerializableExpandableContractResolver(),
+		};
 
-        // https://stackoverflow.com/a/36643545
-        private sealed class SerializableExpandableContractResolver : DefaultContractResolver
-        {
-            protected override JsonContract CreateContract(Type objectType)
-            {
-                if (TypeDescriptor.GetAttributes(objectType).Contains(new TypeConverterAttribute(typeof(ExpandableObjectConverter))))
-                    return CreateObjectContract(objectType);
-                return base.CreateContract(objectType);
-            }
-        }
-    }
+		// https://stackoverflow.com/a/36643545
+		private sealed class SerializableExpandableContractResolver : DefaultContractResolver
+		{
+			protected override JsonContract CreateContract(Type objectType)
+			{
+				if (TypeDescriptor.GetAttributes(objectType).Contains(new TypeConverterAttribute(typeof(ExpandableObjectConverter))))
+					return CreateObjectContract(objectType);
+				return base.CreateContract(objectType);
+			}
+		}
+	}
 
-    public static class BotContainer
-    {
-        public static void RunBots(ProgramConfig prog)
-        {
-            IPokeBotRunner env = GetRunner(prog);
-            foreach (var bot in prog.Bots)
-            {
-                bot.Initialize();
-                if (!AddBot(env, bot, prog.Mode))
-                    Console.WriteLine($"Failed to add bot: {bot}");
-            }
+	public static class BotContainer
+	{
+		public static async Task RunBots(ProgramConfig prog)
+		{
+			IPokeBotRunner env = GetRunner(prog);
+			foreach (var bot in prog.Bots)
+			{
+				bot.Initialize();
+				if (!AddBot(env, bot, prog.Mode))
+					Console.WriteLine($"Failed to add bot: {bot}");
+			}
 
-            LogUtil.Forwarders.Add((msg, ident) => Console.WriteLine($"{ident}: {msg}"));
-            env.StartAll();
-            Console.WriteLine($"Started all bots (Count: {prog.Bots.Length}.");
-            Console.WriteLine("Press any key to stop execution and quit. Feel free to minimize this window!");
-            Console.ReadKey();
-            env.StopAll();
-        }
+			LogUtil.Forwarders.Add((msg, ident) => Console.WriteLine($"{ident}: {msg}"));
+			env.StartAll();
+			Console.WriteLine($"Started all bots (Count: {prog.Bots.Length}.");
+			await Task.Delay(-1);
+		}
 
-        private static IPokeBotRunner GetRunner(ProgramConfig prog) => prog.Mode switch
-        {
-            ProgramMode.SWSH => new PokeBotRunnerImpl<PK8>(prog.Hub, new BotFactory8()),
-            ProgramMode.BDSP => new PokeBotRunnerImpl<PB8>(prog.Hub, new BotFactory8BS()),
-            ProgramMode.LA => new PokeBotRunnerImpl<PA8>(prog.Hub, new BotFactory8LA()),
-            ProgramMode.SCVI => new PokeBotRunnerImpl<PK9>(prog.Hub, new BotFactory9SV()),
-            _ => throw new IndexOutOfRangeException("Unsupported mode."),
-        };
+		private static IPokeBotRunner GetRunner(ProgramConfig prog) => prog.Mode switch
+		{
+			ProgramMode.SWSH => new PokeBotRunnerImpl<PK8>(prog.Hub, new BotFactory8()),
+			ProgramMode.BDSP => new PokeBotRunnerImpl<PB8>(prog.Hub, new BotFactory8BS()),
+			ProgramMode.LA => new PokeBotRunnerImpl<PA8>(prog.Hub, new BotFactory8LA()),
+			ProgramMode.SCVI => new PokeBotRunnerImpl<PK9>(prog.Hub, new BotFactory9SV()),
+			_ => throw new IndexOutOfRangeException("Unsupported mode."),
+		};
 
-        private static bool AddBot(IPokeBotRunner env, PokeBotState cfg, ProgramMode mode)
-        {
-            if (!cfg.IsValid())
-            {
-                Console.WriteLine($"{cfg}'s config is not valid.");
-                return false;
-            }
+		private static bool AddBot(IPokeBotRunner env, PokeBotState cfg, ProgramMode mode)
+		{
+			if (!cfg.IsValid())
+			{
+				Console.WriteLine($"{cfg}'s config is not valid.");
+				return false;
+			}
 
-            PokeRoutineExecutorBase newBot;
-            try
-            {
-                newBot = env.CreateBotFromConfig(cfg);
-            }
-            catch
-            {
-                Console.WriteLine($"Current Mode ({mode}) does not support this type of bot ({cfg.CurrentRoutineType}).");
-                return false;
-            }
-            try
-            {
-                env.Add(newBot);
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-                return false;
-            }
+			PokeRoutineExecutorBase newBot;
+			try
+			{
+				newBot = env.CreateBotFromConfig(cfg);
+			}
+			catch
+			{
+				Console.WriteLine($"Current Mode ({mode}) does not support this type of bot ({cfg.CurrentRoutineType}).");
+				return false;
+			}
+			try
+			{
+				env.Add(newBot);
+			}
+			catch (ArgumentException ex)
+			{
+				Console.WriteLine(ex.Message);
+				return false;
+			}
 
-            Console.WriteLine($"Added: {cfg}: {cfg.InitialRoutine}");
-            return true;
-        }
-    }
+			Console.WriteLine($"Added: {cfg}: {cfg.InitialRoutine}");
+			return true;
+		}
+	}
 }
